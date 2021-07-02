33ca8907fdc3957074956a6971cba2cd084286f7
Erubin <magomedamin90@ya.ru>
2021-07-02 11:23:12 +0300

    Initial commit

---
+++ src/test/java/tech/erubin/annyeong_eat/AnnyeongEatApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.erubin.annyeong_eat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnyeongEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

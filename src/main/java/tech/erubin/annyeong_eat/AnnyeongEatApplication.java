3172af3dca72a01f8eec277f5b241b0f3d8e3e32
Erubin <magomedamin90@ya.ru>
2021-07-02 11:15:16 +0300

    Initial commit

---
+++ src/main/java/tech/erubin/annyeong_eat/AnnyeongEatApplication.java
@@ -0,0 +1,13 @@
+package tech.erubin.annyeong_eat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnyeongEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnyeongEatApplication.class, args);
+    }
+
+}

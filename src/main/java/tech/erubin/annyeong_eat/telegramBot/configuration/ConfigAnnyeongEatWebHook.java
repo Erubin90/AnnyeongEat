a957ba8b59b52bda5b67fb82321bb5894741dc38
Erubin <magomedamin90@ya.ru>
2021-07-10 16:18:23 +0300

    add bean messageSource

--- src/main/java/tech/erubin/annyeong_eat/telegramBot/configuration/ConfigAnnyeongEatWebHook.java
+++ src/main/java/tech/erubin/annyeong_eat/telegramBot/configuration/ConfigAnnyeongEatWebHook.java
@@ -3,10 +3,12 @@ package tech.erubin.annyeong_eat.telegramBot.configuration;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import tech.erubin.annyeong_eat.telegramBot.AnnyeongEatWebHook;
 import tech.erubin.annyeong_eat.telegramBot.service.botService.UpdateService;
 
@@ -31,6 +33,15 @@ public class ConfigAnnyeongEatWebHook {
     @Bean
     public AnnyeongEatWebHook getAnnyeongEatWebHook() {
         return new AnnyeongEatWebHook(botUsername, botToken, botPath, updateService);
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages.properties");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 
 }
